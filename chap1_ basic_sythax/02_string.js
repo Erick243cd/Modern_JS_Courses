1fb17ee037ab077c70ce0593793b2e20189f4035
Erick243cd <erickbanze.develop@gmail.com>
2022-02-09 09:24:50 -0800

    Chap1 le Type données [String]

---
+++ chap1_ basic_sythax/02_string.js
@@ -0,0 +1,12 @@
+/**
+ * Le Type de donnees [String]
+ * Le string se definit comme un caractere ou une chaine de caracteres
+ */
+ console.log('A');
+ console.log("Le double cotes sont utilisés pour les chaines qui contiennent des cote");
+/**
+ * Déclaration et Affectation d'une variable de Type String
+ */
+
+let email = 'email@gmail.com';
+console.log(email);
