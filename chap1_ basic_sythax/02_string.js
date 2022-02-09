5cdc64ac2ae8f6fe12b0ce9629d86f1a6f82c3cb
Erick243cd <erickbanze.develop@gmail.com>
2022-02-09 10:04:02 -0800

    Chap1 le Type données [String] Methodes et fonctions liées

--- chap1_ basic_sythax/02_string.js
+++ chap1_ basic_sythax/02_string.js
@@ -10,3 +10,45 @@
 
 let email = 'email@gmail.com';
 console.log(email);
+
+
+/**
+ * La concaténation de strings
+ */
+
+let name = "Erick";
+
+let nomComplet = email +' ' +name
+
+console.log(nomComplet);
+
+
+/**
+ * Obtenir un caractère dans une chaine
+ */
+console.log(nomComplet[2]);
+
+/**
+ * Obetenir la taille de String
+ */
+console.log(nomComplet.length);
+
+/**
+ * Fonctions et Methode d'une String
+ * 
+ * 1. Une fonction est un bloc de code qui effectue une tâche spécifique
+ * 2. Une méthode est une fonction attachée à un Objet ou à un type de données
+ */
+//Methodes sans arguments
+console.log(nomComplet.toLocaleUpperCase());
+console.log(nomComplet.toLowerCase());
+
+//Méthode avec arguments
+
+console.log(nomComplet.indexOf('@')); //Afficher la position qu'occupe @
+console.log(nomComplet.slice(0, 6));//Afficher la tranche
+
+console.log(nomComplet.replace("m", 'Z')); 
+
+//Cfr https://www.w3schools.com/js/js_string_methods.asp pour plus de methodes
+
