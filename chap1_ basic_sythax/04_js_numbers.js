e91bd060f3e7633f15b2c1835e3efe814efc6f33
Erick243cd <erickbanze.develop@gmail.com>
2022-02-26 07:56:39 -0800

    Type Number

---
+++ chap1_ basic_sythax/04_js_numbers.js
@@ -0,0 +1,16 @@
+/**
+ * Le type de données [Number] prends en charge 
+ * tout type de données numérique(integer, double, float, etc)
+ */
+let nbreEntier = 12;
+let nbreDecimal = 13.7;
+
+console.log(nbreEntier);
+
+console.log(typeof(nbreDecimal)); //typeof returne le type de données
+
+/**
+ * Les opérateurs mathématiques [*, -, +, **, %]
+ */
+let likes = 2 * (2**2) + (4/3) % 2
+console.log(likes);
